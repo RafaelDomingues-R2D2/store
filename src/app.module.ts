6093cf226a40966d71e623ee565cf56e083db301
Rafael Domingues - R2D2 <Rafael Domingues - R2D2>
2025-09-24 10:15:09 -0300

    add http exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,11 @@ import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { OrderModule } from './order/order.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './filters/http-exception-filter';
 
 @Module({
   imports: [PrismaModule, UserModule, ProductModule, OrderModule],
+  providers: [{ provide: APP_FILTER, useClass: HttpExceptionFilter }],
 })
 export class AppModule {}
